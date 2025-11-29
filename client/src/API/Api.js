13472b213d57021d552ccf32f4ceddad4a38707b
Hariharan.s <hreehs1428@gmail.com>
2025-11-29 18:48:04 +0530

    api updated

--- client/src/API/Api.js
+++ client/src/API/Api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL:'http://localhost:4343/api'
+    baseURL:'https://taskmanager-jwt-crud-protected-routes.onrender.com/api'
 })
 
 api.interceptors.request.use((config)=>{
